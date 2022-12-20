85b0c99a63c093ea6ff91ff046c3636c0bec5e95
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2022-12-20 08:37:48 -0500

    Initial Commit

---
+++ Flowmodoro/FlowmodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowmodoroApp.swift
+//  Flowmodoro
+//
+//  Created by Ammar Ahmed on 2022-12-20.
+//
+
+import SwiftUI
+
+@main
+struct FlowmodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
